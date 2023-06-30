a02b755258afa21418f5607056db9119fcf93185
desipossa <c1jang@hanmail.net>
2023-06-30 11:59:56 +0900

    20230630

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ rightArrow.addEventListener('click', function () {
 
 const proSlide = new Swiper('.pro_slide', {
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -26,12 +26,35 @@ const proSlide = new Swiper('.pro_slide', {
         type: "progressbar",
     },
 
-    scrollbar: {
-        el: ".drag",
-        draggable: true,
-        dragSize: 150,
-        hide: false,
-    },
+    breakpoints: {
+        540: {
+            slidesPerView: 4,
+        }
+    }
 
+    // scrollbar: {
+    //     el: ".drag",
+    //     draggable: true,
+    //     dragSize: 150,
+    //     hide: false,
+    // },
+
+});
+
+
+const mobileBtn = document.querySelector('.mobile_btn');
+const GNB = document.querySelector('#gnb');
+const H1 = document.querySelector('h1');
+
+mobileBtn.addEventListener('click', function () {
+    GNB.classList.toggle('on');
+    H1.classList.toggle('on');
+});
+
+
+GNB.addEventListener('wheel', function (e) {
+    if (GNB.classList.contains('on')) {
+        e.preventDefault();
+    }
 })
 
